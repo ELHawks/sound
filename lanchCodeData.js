62fb97d8daaa17ae78cca1e1b07d1639b1e6bc69
Elijah Hawks <97473011+ELHawks@users.noreply.github.com>
2022-03-28 14:27:43 -0700

    Add files via upload

---
+++ lanchCodeData.js
@@ -0,0 +1,50 @@
+// these funtions ans classes will allow the daate on the table to update evey 5 seconds
+
+class InputData{
+    constructor(
+        time_seconds,
+        latitdiude,
+        longitude,
+        gps_altitude,
+        bmp_alititude,
+        bmpsensor_pressure,
+        bmpsensor_temp,
+        digsensor_temp,
+        cssSensor_temp,
+        cssSensor_eCO2,
+        cssSensor_TVDC,
+        UV,
+        accelx,
+        accelY,
+        accelZ,
+        magmeticX,
+        magneticY,
+        magneticZ,
+        gyroX,
+        gyroY,
+        gyroZ,
+
+    ){
+        this.time_seconds=time_seconds;
+        this.latitdiude=latitdiude;
+        this.longitude=longitude;
+        this.gps_altitude=gps_altitude;
+        this.bmp_alititude=bmp_alititude;
+        this.bmpsensor_pressure=bmpsensor_pressure;
+        this.bmpsensor_temp=bmpsensor_temp;
+        this.digsensor_temp=digsensor_temp;
+        this.cssSensor_temp=cssSensor_temp;
+        this.cssSensor_eCO2=cssSensor_eCO2;
+        this.cssSensor_TVDC=cssSensor_TVDC;
+        this.UV=UV;
+        this.accelx=accelx;
+        this.accelY=accelY;
+        this.accelZ=accelZ;
+        this.magmeticX=magmeticX;
+        this.magneticY=magneticY;
+        this.magneticZ=magneticZ;
+        this.gyroX=gyroX;
+        this.gyroY=gyroY;
+        this.gyroZ=gyroZ;
+    }
+}
